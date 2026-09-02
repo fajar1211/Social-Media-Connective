9015cc46e5de039b790ee90d969921241bfc7fab
fajar1211 <fajar1211@users.noreply.github.com>
2026-09-02 19:33:23 +0700

    fix: increase popup cleanup interval to 1000ms for better localStorage polling

--- src/routes/clients.$clientId.tsx
+++ src/routes/clients.$clientId.tsx
@@ -718,7 +718,7 @@ function SettingsTab({ clientId }: { clientId: string }) {
               forceUpdate();
             }
           }
-        }, 500);
+        }, 1000);
       }
     } else if (platform === "Instagram") {
       const width = 600;
@@ -828,7 +828,7 @@ function SettingsTab({ clientId }: { clientId: string }) {
               forceUpdate();
             }
           }
-        }, 500);
+        }, 1000);
       }
     } else {
       toast.info(`${platform} is coming soon!`);
